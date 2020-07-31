5506704e334c2686f69d2024ff7459180da75eb3
DESKTOP-13H48IC\tomer <tomerevach@gmail.com>
2020-07-31 19:41:44 +0300

    work on budget controller build function constructor for expense and income and data structure to store them

--- Budget App Project/app.js
+++ Budget App Project/app.js
@@ -1,19 +1,43 @@
 /*Im using Module Pattern with closures and IIFE each module is indepandant by his self UI Seprated from Data
-  Each on of the modules need to return object by return {.....}
-  */
+Each on of the modules need to return object by return {.....}
+*/
 
-var budgetController = (function (){                           // Data Controller //
-    return {
-        addBalance: function(amount){
-            return {
-                balance:document.querySelector('.budget__value').value + amount
-            }
+ // Data Controller //
+var budgetController = (function (){  
+    
+    //Function constructors For Expense Items & Income Items//
+    var Expense = function(id , description ,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };        
+
+    var Income = function(id , description ,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;    
+    }; 
+
+    //Data Structure(Object) to store all the data(Expense item , Income item , total exp , total inc//
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        total: {
+            totalExp:0,
+            totalInc:0
         }
+    }
+    return {
+            
     }
 })();
 
-var UIController = (function (){   // Interface Controller // 
-    var DOMStrings = {                                      // Doing this object if we in future want to change name of the classes in html it make our life better by doing it and not change in the entire code//
+// Interface Controller // 
+// Doing this object if we in future want to change name of the classes in html it make our life better by doing it and not change in the entire code//
+var UIController = (function (){   
+    var DOMStrings = {                                      
         Type: '.add__type',
         Description: '.add__description',
         Value: '.add__value',
@@ -35,12 +59,14 @@ var UIController = (function (){   // Interface Controller //
 })();
 
  // Connect between UI and Data Modules //
+ //Because keypress on enter is in the global environment of the window
+ //event paramter is event from Listener when we push button in our app, im going to check his key code by using console.log and see object prortyes and his code:13
+ // its mean Enter pressed keyCode method for normal browser and which methods for older browsers
 var controlCenter = (function(budgetCtrl,UICtrl){   
     function setupEventListeners(){
         document.querySelector(UICtrl.getDOMStrings().buttonV).addEventListener('click',updateItem);
-        document.addEventListener('keypress',function(event){                              //Because keypress on enter is in the global environment of the window
-            //event paramter is event from Listener when we push button in our app, im going to check his key code by using console.log and see object prortyes and his code:13
-        if(event.keyCode === 13 || event.which === 13){                                 // its mean Enter pressed keyCode method for normal browser and which methods for older browsers
+        document.addEventListener('keypress',function(event){                                                                                                           
+        if(event.keyCode === 13 || event.which === 13){                                     
             updateItem();
             }                                                                                 
         })
@@ -49,9 +75,7 @@ var controlCenter = (function(budgetCtrl,UICtrl){
         // 1.Get input data from field 
         var input = UICtrl.getInput();
         // 2.Add item to budget controller
-        budgetCtrl.addBalance(input.value).balance;
         // 3.Add item to UI controller
-
         // 4.Update calculated budget
         // 5.Update UI with calculated budget
     }
