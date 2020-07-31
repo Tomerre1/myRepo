e6123ced9ddf7ee290ac4f2da3b493d68454024c
DESKTOP-13H48IC\tomer <tomerevach@gmail.com>
2020-07-31 18:24:36 +0300

    Step 2 using build keyCode method or which method to recognize when Enter is pressed + add Update function for both events click (V) or press enter

--- Budget App Project/app.js
+++ Budget App Project/app.js
@@ -1,4 +1,4 @@
-/*Im using Module Pattern if closures and IIFE each module is indepandant by his self UI Seprated from Data
+/*Im using Module Pattern with closures and IIFE each module is indepandant by his self UI Seprated from Data
   Each on of the modules need to return object by return {.....}
   */
 
@@ -6,10 +6,25 @@ var budgetController = (function (){                           // Data Controlle
 
 })();
 
-var UIController = (function (){                             // Interface Controller // 
-
+var UIController = (function (){   // Interface Controller // 
+    
 })();
 
-var controlCenter = (function(budgetCtrl,UICtrl){                             // Connect between UI and Data Modules //
+var controlCenter = (function(budgetCtrl,UICtrl){                                             // Connect between UI and Data Modules //
+    function Update(){
+        // 1.Get input data from field 
+        // 2.Add item to budget controller
+        // 3.Add item to UI controller
+        // 4.Update calculated budget
+        // 5.Update UI with calculated budget
+        console.log("Check");
+    }
+    document.querySelector('.add__btn').addEventListener('click',Update);
 
+    document.addEventListener('keypress',function(event){                              //Because keypress on enter is in the global environment of the window
+                                                                                     //event paramter is event from Listener when we push button in our app, im going to check his key code by using console.log and see object prortyes and his code:13
+    if(event.keyCode === 13 || event.which === 13){                                 // its mean Enter pressed keyCode method for normal browser and which methods for older browsers
+        Update();
+    }                                                                                 
+    })
 })(budgetController,UIController);
